b97481f98ad2fc68b8f38813f45e7172c588db2a
Pedro Heliodoro <webmaster@ideiasfrescas.com>
2019-11-14 13:37:28 +0000

    encontrado o teste especifico para um random point

--- _test_turf.js
+++ _test_turf.js
@@ -1,5 +1,8 @@
-import * as turf from '@turf/helpers'
-var t = turf.multiPolygon( [ [5.39014, 43.279295], [5.390709, 43.278749], [5.3909, 43.2785], [5.39014, 43.279295] ], [ [5.379856, 43.252967], [5.422988, 43.249466], [5.425048, 43.245153], [5.379856, 43.252967] ] );
-console.log(t);
+var turf = require('@turf/turf');
+//import * as turf from '@turf/helpers'
+var t = turf.polygon( [[ [5.39014, 43.279295], [5.390709, 43.278749], [5.3909, 43.2785], [5.39014, 43.279295] ], [ [5.379856, 43.252967], [5.422988, 43.249466], [5.425048, 43.245153], [5.379856, 43.252967] ] ]);
+//console.log(t);
 
+var points = turf.randomPoint(1, {bbox: [5.39014, 43.279295, 5.379856, 43.252967]});
+console.log(points.features[0].geometry.coordinates);
 ///var turfpolygon = turf.multiPolygon(this.polygons);
