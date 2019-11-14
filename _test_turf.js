d57ff720176c58ccc8e93a5cef66213d74643c8d
Pedro Heliodoro <webmaster@ideiasfrescas.com>
2019-11-14 13:23:13 +0000

    adicionar testes para turf

---
+++ _test_turf.js
@@ -0,0 +1,5 @@
+import * as turf from '@turf/helpers'
+var t = turf.multiPolygon( [ [5.39014, 43.279295], [5.390709, 43.278749], [5.3909, 43.2785], [5.39014, 43.279295] ], [ [5.379856, 43.252967], [5.422988, 43.249466], [5.425048, 43.245153], [5.379856, 43.252967] ] );
+console.log(t);
+
+///var turfpolygon = turf.multiPolygon(this.polygons);
